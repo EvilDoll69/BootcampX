f9304d555e931ab9d560c7223df714e43eab9ac4
EvilDoll69 <tetiana.fediushyn@gmail.com>
2022-01-04 15:09:43 +0000

    Practicing GROUP and HAVING

--- 2_queries_joins/2_total_assignments_duration_allstudents.sql
+++ 2_queries_joins/2_total_assignments_duration_allstudents.sql
@@ -3,3 +3,12 @@ FROM assignment_submissions
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name = 'FEB12';
+
+
+-- TOTAL SUBMISSION FOR EACH STUDENT --
+-- SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- WHERE students.end_date IS NULL
+-- GROUP BY students.name
+-- HAVING count(assignment_submissions.*) > 250;
