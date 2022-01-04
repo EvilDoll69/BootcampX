ad5c4c14777d357689c6e0e28f7359aa31f92439
EvilDoll69 <tetiana.fediushyn@gmail.com>
2022-01-04 20:14:20 +0000

    New tables were added

--- 3_queries_group_by/3_low_average_completion_time.sql
+++ 3_queries_group_by/3_low_average_completion_time.sql
@@ -1,11 +1,42 @@
-SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
-FROM students
-JOIN assignment_submissions ON student_id = students.id
-JOIN assignments ON assignments.id = assignment_id
-WHERE end_date IS NULL
-GROUP BY student
-HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
-ORDER BY average_assignment_duration;
+-- SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
+-- FROM students
+-- JOIN assignment_submissions ON student_id = students.id
+-- JOIN assignments ON assignments.id = assignment_id
+-- WHERE end_date IS NULL
+-- GROUP BY student
+-- HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+-- ORDER BY average_assignment_duration;
 
 
 
+-- SELECT (
+--   SELECT count(assignments)
+--   FROM assignments
+-- )-count(assignment_submissions) as total_incomplete
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- WHERE students.name = 'Ibrahim Schimmel';
+
+-- SELECT count(students) as total_students
+-- FROM students
+-- JOIN cohorts ON cohorts.id = cohort_id
+-- GROUP BY cohorts;
+
+-- SELECT avg(total_students) as average_total
+-- FROM (
+--   SELECT count(students) as total_students
+--   FROM students
+--   JOIN cohorts ON cohorts.id = cohort_id
+--   GROUP BY cohorts
+-- ) as totals_tables;
+
+SELECT assignments.name 
+FROM assignments
+WHERE id NOT IN
+(
+  SELECT assignment_id
+  FROM assignment_submissions
+  JOIN students ON students.id = student_id
+  WHERE students.name = 'Ibrahim Schimmel'
+);
+ 
