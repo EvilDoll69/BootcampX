cb905d642fbd849ff2433369a21be5d7619583ff
EvilDoll69 <tetiana.fediushyn@gmail.com>
2022-01-05 01:55:07 +0000

    Work with JOIN, DISTINCT

---
+++ 4_queries/4_total_cohort_assistance_duration.sql
@@ -0,0 +1,13 @@
+SELECT cohorts.name as cohort, sum(completed_at-started_at) as total_duration
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts on cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY total_duration;
+
+-- SELECT cohorts.name as cohort, AVG(started_at - created_at) as total_duration
+-- FROM assistance_requests
+-- JOIN  students ON students.id = student_id
+-- JOIN cohorts ON cohorts.id = cohort_id
+-- GROUP BY cohorts.name
+-- ORDER BY total_duration;
